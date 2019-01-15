b0a22bd2da46713527a6c31bde184877f4f9f6e8
Stevan Mihalic <stevanmihalic@gmail.com>
2019-01-15 18:21:37 +0100

    Kreirane klase za zadatke closes #3

---
+++ src/main/java/zadaci/Konstante.java
@@ -0,0 +1,7 @@
+package zadaci;
+
+public class Konstante {
+
+    public static final String DATABASE_URL="jdbc:sqlite:restoran.db";
+
+}
---
+++ src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -0,0 +1,36 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
+public class Zadatak2DodavanjeVrednosti {
+
+    public static void main(String[] args) {
+
+        ConnectionSource connectionSource = null;
+
+        try {
+            connectionSource = new JdbcConnectionSource(Konstante.DATABASE_URL);
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+    }
+
+}
